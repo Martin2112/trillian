7af6adecb502759774e9bb26ac9d30d1b82437e6
Martin Smith <mhs@google.com>
2017-02-20 11:18:14 +0000

    Add citus replication / distribution sql script

---
+++ storage/pgsql/citus_storage.sql
@@ -0,0 +1,27 @@
+-- Creates the replication information to use the schema with CitusDB. Run the
+-- storage.sql file first to create the tables.
+--
+-- Note: This is creating a 'per tenant' scheme where the data per tree is colocated.
+-- This seems to be the only workable option but probably places scalability limits
+-- on each tree.
+
+-- These should be reference tables but it's not allowed for them to have foreign keys or
+-- be a target for foreign keys so it doesn't work.
+SELECT create_distributed_table('trees', 'treeid');
+SELECT create_distributed_table('treecontrol', 'treeid', colocate with => 'trees');
+
+-- Basic Tree Storage
+SELECT create_distributed_table('subtree', 'treeid', colocate_with => 'trees');
+SELECT create_distributed_table('treehead', 'treeid', colocate_with => 'trees');
+
+-- Log Specific
+SELECT create_distributed_table('leafdata', 'treeid', colocate_with => 'trees');
+SELECT create_distributed_table('sequencedleafdata', 'treeid', colocate_with => 'trees');
+
+-- This doesn't need to be colocated and can use a different distribution column. However
+-- it might make sense to do so - this will be investigated at some point
+SELECT create_distributed_table('unsequenced', 'leafidentityhash');
+
+-- Map Specific
+SELECT create_distributed_table('mapleaf', 'treeid', colocate_with => 'trees');
+SELECT create_distributed_table('maphead', 'treeid', colocate_with => 'trees');
