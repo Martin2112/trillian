9404969e7afaf9d89aa78789ea326930e008b413
Martin Smith <mhs@google.com>
2017-04-13 09:35:05 +0100

    Add supprt for marshalling variable args in get subtrees for dbs
    That have positional parameters.

--- storage/sql/coresql/tree_storage.go
+++ storage/sql/coresql/tree_storage.go
@@ -94,6 +94,12 @@ func (t *treeTX) getSubtrees(treeRevision int64, nodeIDs []storage.NodeID) ([]*s
 
 	args := make([]interface{}, 0, len(nodeIDs)+3)
 
+	if !t.ts.wrap.VariableArgsFirst() {
+		args = append(args, interface{}(t.treeID))
+		args = append(args, interface{}(treeRevision))
+		args = append(args, interface{}(t.treeID))
+	}
+
 	// populate args with nodeIDs
 	for _, nodeID := range nodeIDs {
 		if nodeID.PrefixLenBits%8 != 0 {
@@ -105,9 +111,11 @@ func (t *treeTX) getSubtrees(treeRevision int64, nodeIDs []storage.NodeID) ([]*s
 		args = append(args, interface{}(nodeIDBytes))
 	}
 
-	args = append(args, interface{}(t.treeID))
-	args = append(args, interface{}(treeRevision))
-	args = append(args, interface{}(t.treeID))
+	if t.ts.wrap.VariableArgsFirst() {
+		args = append(args, interface{}(t.treeID))
+		args = append(args, interface{}(treeRevision))
+		args = append(args, interface{}(t.treeID))
+	}
 
 	rows, err := stmt.Query(args...)
 	if err != nil {
